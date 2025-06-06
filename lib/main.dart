4350502425ca5f75765420ac2955fcc8e0475e19
Takuto-Adachi <takuada905@gamil.com>
2025-06-06 17:53:25 +0900

    Bluetoothの接続確認画面の作成

--- lib/main.dart
+++ lib/main.dart
@@ -11,112 +11,178 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Conection Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'conection Demo Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // ボタンの処理を表示
+  String buttonOutput = "";
+  // デバイス名リスト
+  List<String> deviceList = [];
+  bool hasCheckedDevice = false;
 
-  void _incrementCounter() {
+  // 赤を表示
+  void showRed() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      hasCheckedDevice = false;
+      buttonOutput = "赤だよー";
     });
+  }
+
+  // 青を表示
+  void showBlue() {
+    setState(() {
+      hasCheckedDevice = false;
+      buttonOutput = "青だよー";
+    });
+  }
+
+  // 緑を表示
+  void showGreen() {
+    setState(() {
+      hasCheckedDevice = false;
+      buttonOutput = "緑だよー";
+    });
+  }
+
+  // 色をリセット
+  void clearColor() {
+    setState(() {
+      hasCheckedDevice = false;
+      buttonOutput = "";
+    });
+  }
+
+  // 接続ボタン
+  void connectDevice() {
+    setState(() {
+      hasCheckedDevice = false;
+      buttonOutput = "デバイス接続";
+    });
+  }
+
+  // グループ作成
+  void decideGroup() {
+    setState(() {
+      hasCheckedDevice = false;
+      buttonOutput = "グループ確定！！";
+    });
+  }
+
+  // 接続デバイス確認ボタン
+  void checkDevice() {
+    setState(() {
+      hasCheckedDevice = true;
+      deviceList = ["デバイスA", "デバイスB", "デバイスC"];
+      buttonOutput = "デバイス確認";
+    });
+  }
+
+  // 丸ボタンの作成
+  Widget _buildCircleButton(String label, VoidCallback onPressed, Color color) {
+    return ElevatedButton(
+      onPressed: onPressed, //押された時の処理
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color, // 背景色を指定
+        foregroundColor: Colors.white, //文字色
+        shape: const CircleBorder(
+          //丸型
+          side: BorderSide(color: Colors.black, width: 1),
+        ),
+        padding: const EdgeInsets.all(36),
+      ),
+      child: Text(label),
+    );
+  }
+
+  Widget _buildBoxButton(String label, VoidCallback onPressed) {
+    return ElevatedButton(onPressed: onPressed, child: Text(label));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('テスト10'),
             Text(
-              '$_counter',
+              '$buttonOutput',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+
+            if(hasCheckedDevice)
+              Expanded(
+                child: deviceList.isNotEmpty
+                  ? ListView.builder(
+                    itemCount: deviceList.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+                        child: Text(
+                          deviceList[index],
+                          style: const TextStyle(fontSize: 18),
+                        ),  
+                      );
+                    },
+                  )
+                  : const Center(
+                      child: Text(
+                        "接続デバイスなし",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildCircleButton("", showRed, Colors.red),
+              const SizedBox(width: 24),
+              _buildCircleButton("", showBlue, Colors.blue),
+              const SizedBox(width: 24),
+              _buildCircleButton("", showGreen, Colors.green),
+              const SizedBox(width: 24),
+              _buildCircleButton("", clearColor, Colors.grey),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildBoxButton("接続", connectDevice),
+              const SizedBox(width: 24),
+              _buildBoxButton("グループの決定", decideGroup),
+              const SizedBox(width: 24),
+              _buildBoxButton("接続確認", checkDevice),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
