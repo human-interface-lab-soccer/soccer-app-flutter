85456c3da2be9290ceb853633d662732813b583e
naokeyn <naokey-1228@outlook.jp>
2025-06-09 20:29:22 +0900

    feat: Integrate Firebase into the Flutter project
    - Added GoogleService-Info.plist and Google Services.json for Firebase configuration.
    - Updated iOS and macOS project files to include Firebase dependencies.
    - Modified Info.plist to support indirect input events and disable minimum frame duration on phone.
    - Updated Podfile and Podfile.lock to include Firebase dependencies.
    - Added firebase_core package to pubspec.yaml and updated pubspec.lock.
    - Implemented Firebase initialization in the generated_plugin_registrant files for macOS and Windows.
    - Created firebase_options.dart for Firebase configuration across platforms.
    - Updated versioning in pubspec.yaml to reflect development version.
    - Added change_app_package_name package for Android app package name management.

---
+++ android/app/src/main/kotlin/jp/ac/saitama_u/mech/human/soccerAppFlutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package jp.ac.saitama_u.mech.human.soccerAppFlutter-u.mech.human.soccerAppFlutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/jp/ac/saitamau/mech/human/soccer_app_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/jp/ac/saitamau/mech/human/soccer_app_flutter/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.soccer_app_flutter
+package jp.ac.saitamau.mech.human.soccer_app_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
