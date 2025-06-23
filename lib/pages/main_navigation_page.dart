ece2e58a2e1f6cbbabf91cb826be90f68af1460a
HideyukiNagashio <hidexxx1128@gmail.com>
2025-06-23 23:30:05 +0900

    enumでNavigationItemsをまとめる

--- lib/pages/main_navigation_page.dart
+++ lib/pages/main_navigation_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:soccer_app_flutter/pages/menu_page.dart';
 import 'package:soccer_app_flutter/pages/connection_page.dart';
 import 'package:soccer_app_flutter/pages/note_page.dart';
+
 enum NavigationItems {
   menu(Icons.menu_book, "メニュー", MenuPage()),
   connection(Icons.bluetooth_connected, "接続", ConnectionPage()),
@@ -21,13 +22,7 @@ class MainNavigationPage extends StatefulWidget {
 }
 
 class MainNavigationPageState extends State<MainNavigationPage> {
-  int _selectedIndex = 1; // デフォルトは接続画面（インデックス1）
-
-  final List<Widget> _pages = [
-    const MenuPage(),
-    const ConnectionPage(),
-    const NotePage(),
-  ];
+  int _selectedIndex = NavigationItems.connection.index; // デフォルトは接続画面（インデックス1）
 
   void _onItemTapped(int index) {
     setState(() {
@@ -38,16 +33,14 @@ class MainNavigationPageState extends State<MainNavigationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_selectedIndex],
+      body: NavigationItems.values[_selectedIndex].page,
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.menu_book), label: 'メニュー'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bluetooth_connected),
-            label: '接続',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: '自由帳'),
-        ],
+        items: NavigationItems.values.map((item) {
+          return BottomNavigationBarItem(
+            icon: Icon(item.icon),
+            label: item.label,
+          );
+        }).toList(),
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
       ),
