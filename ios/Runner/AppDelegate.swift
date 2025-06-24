acc304a241e0f36877c1d4dd6281457c1b7170dd
naokeyn <89127745+naokeyn@users.noreply.github.com>
2025-06-24 19:28:37 +0900

    refactor: update AppDelegate structure and remove deprecated nRFMeshManager code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,7 +3,10 @@ import UIKit
 import NordicMesh
 
 @main
-@objc class MeshManager: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
+    
+    private var meshNetworkManager: MeshNetworkManager?
+    
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -30,45 +33,6 @@ import NordicMesh
     
     private func scanMeshNodes(result: FlutterResult) {
         let devices: Array<String> = ["Device-A", "Device-B", "Device-C"]
-         result(devices)
+        result(devices)
     }
 }
-
-//@objc class AppDelegate: FlutterAppDelegate {
-//  override func application(
-//    _ application: UIApplication,
-//    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//  ) -> Bool {
-//
-//    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-//    let batteryChannel = FlutterMethodChannel(
-//      name: "human.mech.saitama-u.ac.jp/battery",
-//      binaryMessenger: controller.binaryMessenger)
-//    batteryChannel.setMethodCallHandler({
-//      [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
-//      // This method is invoked on the UI thread.
-//      guard call.method == "getBatteryLevel" else {
-//        result(FlutterMethodNotImplemented)
-//        return
-//      }
-//      self?.receiveBatteryLevel(result: result)
-//    })
-//
-//    GeneratedPluginRegistrant.register(with: self)
-//    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-//  }
-//
-//  private func receiveBatteryLevel(result: FlutterResult) {
-//    let device = UIDevice.current
-//    device.isBatteryMonitoringEnabled = true
-//    if device.batteryState == UIDevice.BatteryState.unknown {
-//      result(
-//        FlutterError(
-//          code: "UNAVAILABLE",
-//          message: "Battery level not available.",
-//          details: nil))
-//    } else {
-//      result(Int(device.batteryLevel * 100))
-//    }
-//  }
-//}
