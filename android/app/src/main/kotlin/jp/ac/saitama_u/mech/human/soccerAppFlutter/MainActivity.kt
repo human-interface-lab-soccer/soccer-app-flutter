7b4d92c30415687d9c21f23f5cc3c3ac7c06af02
naokeyn <89127745+naokeyn@users.noreply.github.com>
2025-06-28 23:52:57 +0900

    feat: replace battery level method with scanner method channels for improved scanning functionality

--- android/app/src/main/kotlin/jp/ac/saitama_u/mech/human/soccerAppFlutter/MainActivity.kt
+++ android/app/src/main/kotlin/jp/ac/saitama_u/mech/human/soccerAppFlutter/MainActivity.kt
@@ -4,45 +4,25 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import android.content.Context
-import android.content.ContextWrapper
-import android.content.Intent
-import android.content.IntentFilter
-import android.os.BatteryManager
-import android.os.Build.VERSION
-import android.os.Build.VERSION_CODES
+import io.flutter.plugin.common.EventChannel
 
 class MainActivity: FlutterActivity() {
-  private val CHANNEL = "human.mech.saitama-u.ac.jp/battery"
+  private val SCANNER_EVENT_CHANNEL = "human.mech.saitama-u.ac.jp/scannerEventChannel"
+  private val SCANNER_METHOD_CHANNEL = "human.mech.saitama-u.ac.jp/scannerMethodChannel"
 
   override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
     super.configureFlutterEngine(flutterEngine)
-    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, SCANNER_METHOD_CHANNEL).setMethodCallHandler {
       call, result ->
-      if (call.method == "getBatteryLevel") {
-        val batteryLevel = getBatteryLevel()
-
-        if (batteryLevel != -1) {
-          result.success(batteryLevel)
-        } else {
-          result.error("UNAVAILABLE", "Battery level not available.", null)
+      when (call.method) {
+        "startScanning" -> {
+          result.success("[MethodChannel] Scanning started")
         }
-      } else {
-        result.notImplemented()
+        "stopScanning" -> {
+          result.success("[MethodChannel] Scanning stopped")
+        }
+        else -> result.notImplemented()
       }
     }
-  }
-
-  private fun getBatteryLevel(): Int {
-    val batteryLevel: Int
-    if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-      val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
-      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
-    } else {
-      val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
-      batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
-    }
-
-    return batteryLevel
   }
 }
