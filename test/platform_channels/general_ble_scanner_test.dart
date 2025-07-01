d67cd9eaf57feecc7cee0357f4bc3724b8dba800
naokeyn <89127745+naokeyn@users.noreply.github.com>
2025-07-01 17:59:36 +0900

    refactor: update method channel and event channel naming in GeneralBleScanner tests

--- test/platform_channels/general_ble_scanner_test.dart
+++ test/platform_channels/general_ble_scanner_test.dart
@@ -3,26 +3,33 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:soccer_app_flutter/features/platform_channels/general_ble_scanner.dart';
 
 void main() {
-  const MethodChannel methodChannel = MethodChannel(
+  TestWidgetsFlutterBinding.ensureInitialized();
+  const MethodChannel scannerMethodChannel = MethodChannel(
     'human.mech.saitama-u.ac.jp/scannerMethodChannel',
   );
-  const EventChannel eventChannel = EventChannel(
+  const EventChannel scannerEventChannel = EventChannel(
     'human.mech.saitama-u.ac.jp/scannerEventChannel',
   );
 
   group('GeneralBleScanner', () {
     late GeneralBleScanner scanner;
+    bool startScanningCalled = false;
+    bool stopScanningCalled = false;
 
     setUp(() {
+      startScanningCalled = false;
+      stopScanningCalled = false;
       scanner = GeneralBleScanner();
       TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-          .setMockMethodCallHandler(methodChannel, (
+          .setMockMethodCallHandler(scannerMethodChannel, (
             MethodCall methodCall,
           ) async {
             switch (methodCall.method) {
               case 'startScanning':
+                startScanningCalled = true;
                 return null;
               case 'stopScanning':
+                stopScanningCalled = true;
                 return null;
               default:
                 throw MissingPluginException(
@@ -30,14 +37,29 @@ void main() {
                 );
             }
           });
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockStreamHandler(scannerEventChannel, null);
     });
 
     tearDown(() {
       scanner.dispose();
-      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-          .setMockMethodCallHandler(methodChannel, null);
-      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-          .setMockStreamHandler(eventChannel, null);
+      // TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      //     .setMockMethodCallHandler(scannerMethodChannel, null);
+      // TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      //     .setMockStreamHandler(scannerEventChannel, null);
+    });
+
+    test('startScanning calls method channel', () async {
+      expect(startScanningCalled, isFalse);
+      await scanner.startScanning();
+      expect(startScanningCalled, isTrue);
+      expect(stopScanningCalled, isFalse);
+    });
+    test('stopScanning calls method channel', () async {
+      expect(stopScanningCalled, isFalse);
+      await scanner.stopScanning();
+      expect(stopScanningCalled, isTrue);
+      expect(startScanningCalled, isFalse);
     });
   });
 }
